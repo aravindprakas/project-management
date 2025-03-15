020d41f3b5dce322d6786903c12f5ccbd122b8c0
aravindacharyaa <76624209+aravindprakas@users.noreply.github.com>
2025-03-15 16:02:50 +0530

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,48 @@ import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSideBar from "./components/ProjectSideBar";
+import SelectedProjects from "./components/SelectedProjects";
 
 function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectID: undefined,
     projects: [],
+    tasks: [],
   });
+
+  function handleAddTask(text) {
+    setProjectsState((prevState) => {
+      const taskId = Math.random();
+      const newTask = {
+        text: text,
+        projectId: prevState.selectedProjectID,
+        id: taskId,
+      };
+      return {
+        ...prevState,
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
+  }
+
+  function handleDeleteTask(id) {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter((task) => task.id !== id),
+      };
+    });
+  }
+
+  function handleSelectProject(id) {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectID: id,
+      };
+    });
+  }
+
   function handleStartAddProject() {
     setProjectsState((prevState) => {
       return {
@@ -16,7 +52,20 @@ function App() {
       };
     });
   }
-  function handleCancel(){
+
+  function handleDeleteProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectID: undefined,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectID
+        ),
+      };
+    });
+  }
+
+  function handleCancel() {
     setProjectsState((prevState) => {
       return {
         ...prevState,
@@ -24,35 +73,49 @@ function App() {
       };
     });
   }
-   function handleAddProject(projectData){
-    setProjectsState(
-      
-      prevState=>{
-        const newProject = {
-          ...projectData,
-          id: Math.random()
-        };
-        return {
-          ...prevState,
-          selectedProjectID : undefined,
-          projects:[...prevState.projects,newProject]
-        }
-      }
-    )
-   };
 
-   console.log(projectsState);
+  function handleAddProject(projectData) {
+    setProjectsState((prevState) => {
+      const projectId = Math.random();
+      const newProject = {
+        ...projectData,
+        id: projectId,
+      };
+      return {
+        ...prevState,
+        selectedProjectID: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
 
-  let content;
+  const selectedProject = projectsState.projects.find(
+    (project) => project.id === projectsState.selectedProjectID
+  );
 
-  if(projectsState.selectedProjectID === null){
-    content = <NewProject onAdd={handleAddProject} onCancel={handleCancel}/>
-  }else if(projectsState.selectedProjectID === undefined){
-    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  let content = (
+    <SelectedProjects
+      project={selectedProject}
+      onDelete={handleDeleteProject}
+      onAddTask={handleAddTask}
+      onDeleteTask={handleDeleteTask}
+      tasks={projectsState.tasks}
+    />
+  );
+
+  if (projectsState.selectedProjectID === null) {
+    content = <NewProject onAdd={handleAddProject} onCancel={handleCancel} />;
+  } else if (projectsState.selectedProjectID === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
   return (
     <main className="h-screen mt-8 flex gap-8">
-      <ProjectSideBar onStartAddProject={handleStartAddProject} project={projectsState.projects}/>
+      <ProjectSideBar
+        onStartAddProject={handleStartAddProject}
+        project={projectsState.projects}
+        onSelectProject={handleSelectProject}
+        selectedProjectId={projectsState.selectedProjectID}
+      />
       {content}
     </main>
   );
